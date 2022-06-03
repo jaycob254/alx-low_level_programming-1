a47348ad6b75ce8ebbefb1f75ea7486583c8c590
Asmaalamin93 <achimira.93@gmail.com>
2022-06-03 13:42:47 -0700

    my commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,12 @@
  */
 int main(void)
 {
-
-char ch;
-
-for (ch = 'a'; ch <= 'z'; ch++)
-{
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 	putchar(ch);
-}
-putchar('\n')
-return 0
+	}
+	putchar('\n');
+	return 0
 }
