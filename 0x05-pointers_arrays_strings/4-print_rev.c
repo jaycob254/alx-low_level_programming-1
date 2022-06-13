5f234158e6a1e055710d49813cd01d0f36121825
Asmaalamin93 <achimira.93@gmail.com>
2022-06-13 10:37:12 -0700

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	char *p = s;
-
-	while (*s)
-		*s++;
-	return (s - p);
-}
-
-/**
  * print_rev - prints a string in reverse
  * @s: string s
  */
