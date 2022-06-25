4fa59d76f02719fc57fae706e5178baff1c0039b
Asmaalamin93 <achimira.93@gmail.com>
2022-06-24 23:24:38 -0700

    it worked

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * _isalpha - checks for alphabetic characters
+ * @c: character to be checked
+ * Return: 1 if c is an alphabet, 0 otherwise.
  */
-int _islower(int c)
+
+int _isalpha(int c)
 {
-	return (c >= 'a' && c <= 'z');
+       	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	       	return (1);
+	else 
+		return (0);
 }
